9943ebda370009e6ec4f9ad93be36be84b27a6d0
Alicia Prather <Alprather@gmail.com>
2016-03-01 17:33:10 +0400

    username log in working

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,23 @@
 class User < ActiveRecord::Base
+  attr_accessor :login
+  attr_writer :login
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable,
+         authentication_keys: [:login]
+  validates_format_of :username, with: /^[a-zA-Z0-9_\.]*$/, multiline: true
+
+  def login
+    @login || username || email
+  end
+
+  def self.find_for_database_authentication(warden_conditions)
+     conditions = warden_conditions.dup
+     if login = conditions.delete(:login)
+       where(conditions.to_hash).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+     elsif conditions.has_key?(:username) || conditions.has_key?(:email)
+       where(conditions.to_hash).first
+     end
+   end
 end
