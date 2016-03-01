ee71164e7dd446e299ac5df8455598e535b961c3
Alicia Prather <Alprather@gmail.com>
2016-03-01 19:01:04 +0400

    Wikis crud complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
 
-
   devise_for :users
   resources :users, only: [:show, :index] do
+    resources :wikis
   end
+resources :wikis
+
   root 'welcome#index'
 
 end
